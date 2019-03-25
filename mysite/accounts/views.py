2bffddd8011d995a5535292460eabf27931ac8b7
emethg <34809579+emethg@users.noreply.github.com>
2019-03-26 00:45:23 +0200

    reset_mail

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -22,7 +22,6 @@ def register(request):
         return render(request, 'accounts/reg_form.html', args)
 
 
-
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/accounts/'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
