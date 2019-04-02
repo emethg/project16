fc98eaf535736bb0c145e57d8927ce8c0a1e3520
emethg <34809579+emethg@users.noreply.github.com>
2019-04-02 21:22:19 +0300

    test_issue

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('profile/', views.view_profile, name='view_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('change-password/', views.change_password, name='change_password'),
-    path('reset_password/', PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password/', PasswordResetView.as_view(template_name='accounts/password_reset_form.html'), name='password_reset'),
     path('reset_password/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset_password/confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password/complete', PasswordResetConfirmView.as_view(), name='password_reset_complete'),
