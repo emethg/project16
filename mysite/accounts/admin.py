95d37ecbe29cc865187979467c3c039d552b8700
emethg <34809579+emethg@users.noreply.github.com>
2019-04-16 14:47:38 +0300

    T16-123
    delete using ModelAdmin

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Product
 
 # Register your models here.
 admin.site.register(UserProfile)
 
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('name_product', 'price', 'description')
+    list_filter = ('name_product', 'price')
+
+    def delete_model(self, request, obj):
+        """
+        Given a model instance delete it from the database.
+        """
+        obj.delete()
+
+    def get_model_perms(self, request):
+        """
+        Return a dict of all perms for this model. This dict has the keys
+        ``add``, ``change``, ``delete``, and ``view`` mapping to the True/False
+        for each of those actions.
+        """
+        return {
+            'add': self.has_add_permission(request),
+            'change': self.has_change_permission(request),
+            'delete': self.has_delete_permission(request),
+            'view': self.has_view_permission(request),
+        }
+
+
+admin.site.register(Product, ProductAdmin)
