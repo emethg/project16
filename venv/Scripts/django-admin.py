0921816f72865cb5b747e731dc273643fe8ac3ed
yaniv1888 <34810001+yaniv1888@users.noreply.github.com>
2019-05-19 16:57:50 +0300

    MenuDish & TipOrganization

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\emeth\PycharmProjects\project16\venv\Scripts\python.exe
+#!C:\Users\yaniv\OneDrive\Documents\GitHub\project16\venv\Scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
